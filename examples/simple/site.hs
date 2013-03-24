6359bbec3fdadd580cb5b12b0d821e418f3e9702
Dobronszki János <janosdobronszki@citromail.hu>
2013-03-24 14:22:16 +0100

    Working now.

--- examples/simple/site.hs
+++ examples/simple/site.hs
@@ -1,9 +1,30 @@
 {-# LANGUAGE OverloadedStrings      #-}
 
 import qualified Haquery as Haq
+import qualified Hakit as H
 import qualified TooEasy as Too
+import qualified Data.Text as T
+import qualified System.Directory as D
+
+siteF = "_site"
 
 main = do
-    posts <- Too.indexify "posts"
+    posts <- Too.loadPosts "posts"
     templ <- Too.loadTemplate "template.html"
-    
+    sitesEx <- D.doesDirectoryExist siteF
+    if sitesEx
+        then return ()
+        else D.createDirectory siteF
+    mapM_ (processPost templ) posts
+    where
+    processPost :: Haq.Tag -> (H.Document, Haq.Tag) -> IO ()
+    processPost templ post = do
+        let fn = T.unpack . H.getString "fileName" $ fst post
+            fileName = siteF ++ "/" ++ fn
+            body = snd post
+            postTitle = T.unpack . H.getString "title" $ fst post
+            result = 
+                Haq.alter ".content" (\_ -> body) $
+                Haq.alter "title" (\_ -> Haq.text . T.pack $ "example site - " ++ postTitle) templ
+        putStr $ "Writing file " ++ fileName ++ "\n"
+        writeFile fileName . T.unpack $ Haq.render result
--- src/tooeasy.hs
+++ src/tooeasy.hs
@@ -12,7 +12,6 @@ import qualified Data.List as L
 import qualified Haquery as Haq
 import qualified Data.ByteString.Char8 as C
 import qualified Data.Text as T
-import Debug.Trace
 
 parseMeta :: String -> H.Document
 parseMeta s =
@@ -21,32 +20,32 @@ parseMeta s =
         fil = map (\xs -> [xs!!0, T.unpack . T.strip $ T.pack (xs!!1)]) $ filter (\xs -> length xs == 2) byColon
     in H.interpretDoc $ map (\xs -> (C.pack (xs!!0), Just $ C.pack (xs!!1))) fil
 
-parseWrap :: T.Text -> Tag
+parseWrap :: T.Text -> Haq.Tag
 parseWrap t =
-    let p = Haq.parseHtml 
-    if length p > 1
-        then Haq.div' [] body'
-        else body 
+    let p = Haq.parseHtml t
+    in if length p > 1
+        then Haq.div' [] p
+        else p!!0
 
 -- | Loads all posts in a given folder.
 loadPosts :: String -> IO [(H.Document, Haq.Tag)]
 loadPosts dir = do
     fnames' <- D.getDirectoryContents dir
     let fnames = filter (not . L.isPrefixOf ".") fnames'
-    mapM (\fn -> (readFile $ dir ++ "/" ++ fn) >>= \x -> return $ process x fn) $ trace (show fnames) fnames
+    mapM (\fn -> (readFile $ dir ++ "/" ++ fn) >>= \x -> return $ process x fn) fnames
     where
     process :: String -> String -> (H.Document, Haq.Tag)
     process str fileName =
         let s = Spl.splitOn "---" str
-            body :: [Haq.Tag]
+            body :: Haq.Tag
             body = parseWrap . T.pack $ L.intercalate "" $ tail s
             meta = parseMeta (s!!0)
         in if length s < 2
-            then error $ "Can't find metadata in file " ++ str
+            then error $ "Can't find metadata in file " ++ fileName
             -- We wrap the body in a div if it is more than one tag.
             else (H.set "fileName" fileName meta, body)
 
 -- | Loads a single template file.
 loadTemplate :: String -> IO Haq.Tag
-loadTemplate fileName = parseWrap $ readFile fileName
+loadTemplate fileName = readFile fileName >>= \f -> return . parseWrap $ T.pack f
     
